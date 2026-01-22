a460cd7aa7ec30aa8f1ec9f3d048cae6dc65ccfc
Pepper Hu <pepperannhu@gmail.com>
2026-01-22 23:20:00 +1000

    My attempt for password generation

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 import tkinter as tk
 from tkinter import PhotoImage
 from tkinter import messagebox
+import random
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+
 def generate_password_on_click():
-    pass
+    letter_list = [random.choice(letters) for l in range(nr_letters)]
+    # for char in range(nr_letters):
+    #   password_list.append(random.choice(letters))
+
+    symbol_list = [random.choice(symbols) for s in range(nr_symbols)]
+    # for char in range(nr_symbols):
+    #   password_list += random.choice(symbols)
+
+    num_list = [random.choice(numbers) for n in range(nr_numbers)]
+    # for char in range(nr_numbers):
+    #   password_list += random.choice(numbers)
+
+    password_list = letter_list + symbol_list + num_list
+
+    random.shuffle(password_list)
+
+    auto_password = ""
+    for char in password_list:
+      auto_password += char
+
+    print(f"Your password is: {auto_password}")
+
+    password_entry.delete(0, 'end')
+    password_entry.insert(0, auto_password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_on_click():
     website = website_entry.get()
